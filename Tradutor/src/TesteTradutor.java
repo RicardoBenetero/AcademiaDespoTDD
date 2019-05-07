a601f4955ec00361e9e1a09adea68c7ae20296bb
Ricardo <Ricardo@192.168.0.17>
2019-05-07 20:53:17 -0300

    Hands-on Tradutor com TDD - Parte 3

--- Tradutor/src/TesteTradutor.java
+++ Tradutor/src/TesteTradutor.java
@@ -6,39 +6,49 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TesteTradutor {
-	
+
 	private Tradutor t;
 
 	@Before
 	public void criarTradutor() {
-		
+
 		t = new Tradutor();
 
-		
 	}
 
 	@Test
 	public void tradutorSemPalavras() {
-		
-		
+
 		assertTrue(t.estaVazio());
-		
+
 	}
+
 	@Test
 	public void umaTraducao() {
-		
+
 		t.adicionaTraducao("bom", "good");
 		assertFalse(t.estaVazio());
-		assertEquals("good",t.traduzir("bom"));
-		
+		assertEquals("good", t.traduzir("bom"));
+
 	}
+
 	@Test
 	public void duasTraducoes() {
-		
+
 		t.adicionaTraducao("bom", "good");
 		t.adicionaTraducao("mau", "bad");
-		assertEquals("good",t.traduzir("bom"));
-		assertEquals("bad",t.traduzir("mau"));
+		assertEquals("good", t.traduzir("bom"));
+		assertEquals("bad", t.traduzir("mau"));
+
+	}
+
+	@Test
+	public void duasTraduzirFrase() {
+
+		t.adicionaTraducao("guerra", "war");
+		t.adicionaTraducao("é", "is");
+		t.adicionaTraducao("ruim", "bad");
+		assertEquals("war is bad", t.traduzirFrase("guerra é ruim"));
 
 	}
 
--- Tradutor/src/Tradutor.java
+++ Tradutor/src/Tradutor.java
@@ -2,23 +2,34 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Tradutor {
-	
-	
-	private Map<String,String> traducoes = new HashMap<>();
+
+	private Map<String, String> traducoes = new HashMap<>();
 
 	public boolean estaVazio() {
-		
+
 		return traducoes.isEmpty();
 	}
 
 	public void adicionaTraducao(String palavra, String traducao) {
 		this.traducoes.put(palavra, traducao);
-		
+
 	}
 
 	public String traduzir(String palavra) {
-		
+
 		return traducoes.get(palavra);
+	}
+
+	public String traduzirFrase(String frase) {
+		String[] palavras = frase.split(" ");
+		String fraseTraduzida = "";
+		for (String palavra : palavras) {
+			String traducao = traduzir(palavra);
+			fraseTraduzida += " " + traducao;
+
+		}
+
+		return fraseTraduzida.trim();
 	}
 
 }
