d7f98c46da8734f599b732ec316068fc08fed16f
Ricardo <Ricardo@192.168.0.17>
2019-05-09 21:08:45 -0300

    Implementação

--- ExercicioCamelCase/src/br/courser/TransformaEmCamelCase.java
+++ ExercicioCamelCase/src/br/courser/TransformaEmCamelCase.java
@@ -3,8 +3,11 @@ package br.courser;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import br.coursera.test.InicioDigitoException;
+import br.coursera.test.PossuiCaracterEspecialException;
 
 public class TransformaEmCamelCase {
 
@@ -12,15 +15,26 @@ public class TransformaEmCamelCase {
 		TransformaEmCamelCase converter = new TransformaEmCamelCase();
 		List<String> listaPalavras = null;
 		if (converter.comecaComDigito(original)) {
-			throw new InicioDigitoException("A palavra inicia com digito");
+			throw new InicioDigitoException("A palavra não deve começar com números");
+		} else if (converter.possuiCaracteresEspeciais(original)) {
+			throw new PossuiCaracterEspecialException(
+					"Caracteres especiais não são permitidos, somente letras e números");
 		} else {
 			listaPalavras = converter.retornaLista(original);
 		}
 		return listaPalavras;
 	}
+
+	private boolean possuiCaracteresEspeciais(String original) {
+		Pattern p = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
+		Matcher m = p.matcher(original);
+		return m.find();
+	}
+
 	private boolean comecaComDigito(String original) {
 		return Character.isDigit(original.charAt(0));
 	}
+
 	private List<String> retornaLista(String original) {
 		List<String> listaPalavras;
 		if (this.isAcronimo(original)) {
@@ -32,9 +46,11 @@ public class TransformaEmCamelCase {
 		}
 		return listaPalavras;
 	}
+
 	private boolean isAcronimo(String original) {
 		return original.toUpperCase().equals(original);
 	}
+
 	private List<String> retornaListaDePalavras(List<String> listaPalavras) {
 		if (this.verificaSeListaPossuiUmaPalavra(listaPalavras)) {
 			this.converteParaMinusculoQuandoUmaPalavra(listaPalavras);
@@ -43,6 +59,7 @@ public class TransformaEmCamelCase {
 		}
 		return listaPalavras;
 	}
+
 	private List<String> trataListaPalavras(List<String> listaPalavras) {
 		List<String> novaListaPalavras = new ArrayList<String>();
 		String montarAcronimo = new String();
@@ -54,12 +71,15 @@ public class TransformaEmCamelCase {
 		}
 		return novaListaPalavras;
 	}
+
 	private void converteParaMinusculoQuandoUmaPalavra(List<String> listaPalavras) {
 		listaPalavras.set(0, listaPalavras.get(0).toLowerCase());
 	}
+
 	private boolean verificaSeListaPossuiUmaPalavra(List<String> listaPalavras) {
 		return (listaPalavras.size() == 1);
 	}
+
 	private void incluirPalavra(List<String> listaPalavras, String palavra) {
 		if (listaPalavras.size() == 0)
 			listaPalavras.add(palavra.toLowerCase());
--- ExercicioCamelCase/src/br/coursera/test/PossuiCaracterEspecialException.java
+++ ExercicioCamelCase/src/br/coursera/test/PossuiCaracterEspecialException.java
@@ -1,5 +1,8 @@
 package br.coursera.test;
 
 public class PossuiCaracterEspecialException extends RuntimeException {
+	public PossuiCaracterEspecialException(String msg) {
+		super(msg);
+}
 
 }
