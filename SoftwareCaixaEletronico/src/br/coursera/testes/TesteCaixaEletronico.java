67e56d2433e5751bafdd68415e8c50d9f51522ea
Ricardo <Ricardo@192.168.0.17>
2019-05-14 20:56:06 -0300

    Refatoração

--- SoftwareCaixaEletronico/src/br/coursera/testes/TesteCaixaEletronico.java
+++ SoftwareCaixaEletronico/src/br/coursera/testes/TesteCaixaEletronico.java
@@ -5,38 +5,42 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import br.coursera.CaixaEletronico;
 
 public class TesteCaixaEletronico {
+	
+	CaixaEletronico atm;
+	List<ContaCorrente> contas;
+	MockServicoRemoto mock;
+	ContaCorrente conta1;
+	ContaCorrente conta2;
+
+	
+	@Before
+	public void inicializaDados() {
+		
+		atm = new CaixaEletronico();
+		contas = new ArrayList<ContaCorrente>();
+		conta1 = new ContaCorrente("1234");
+		conta2 = new ContaCorrente("5678");
+		contas.add(conta1);
+		contas.add(conta2);
+		mock = new MockServicoRemoto(contas);
+		
+		
+	}
 
 	@Test
 	public void logarComSucesso() {
-		CaixaEletronico atm = new CaixaEletronico();
-		
-		List<ContaCorrente> contas = new ArrayList<ContaCorrente>();
-		
-		ContaCorrente conta1 = new ContaCorrente("1234");
-		
-		contas.add(conta1);
-
-		MockServicoRemoto mock = new MockServicoRemoto(contas);
 		
 		assertEquals( "Usuário Autenticado", atm.login("1234",mock));
 	}
 	
 	@Test
 	public void logarComSucessoCaixaComVariasContas() {
-		CaixaEletronico atm = new CaixaEletronico();
-		
-		ContaCorrente conta1 = new ContaCorrente("1234");
-		ContaCorrente conta2 = new ContaCorrente("5678");
-		List<ContaCorrente> contas = new ArrayList<ContaCorrente>();
-		contas.add(conta1);
-		contas.add(conta2);
-
-		MockServicoRemoto mock = new MockServicoRemoto(contas);
 		
 		assertEquals( "Usuário Autenticado", atm.login("5678",mock));
 	}
