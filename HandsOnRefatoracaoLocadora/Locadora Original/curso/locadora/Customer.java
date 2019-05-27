d839a4f23fd3329b142ed2d3532bb4a8ef99afbe
Ricardo <Ricardo@192.168.0.17>
2019-05-27 20:34:39 -0300

    Hands-on - Refatorando a Video Locadora - Parte 2

--- HandsOnRefatoracaoLocadora/Locadora Original/curso/locadora/Customer.java
+++ HandsOnRefatoracaoLocadora/Locadora Original/curso/locadora/Customer.java
@@ -26,15 +26,10 @@ public class Customer {
 		Enumeration rentals = _rentals.elements();
 		String result = "Rental Record for " + getName() + "\n";
 		while (rentals.hasMoreElements()) {
-			double thisAmount = 0;
+			
 			Rental each = (Rental) rentals.nextElement();
-			thisAmount = getAmount(each);
-			//add frequent renter points
-			frequentRenterPoints++;
-			//add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-					&& each.getDaysRented() > 1)
-				frequentRenterPoints++;
+			double thisAmount = getAmount(each);
+			frequentRenterPoints += getFrequentRenterPoints(each);
 			//show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t"
 					+ String.valueOf(thisAmount) + "\n";
@@ -45,6 +40,14 @@ public class Customer {
 		result += "You earned " + String.valueOf(frequentRenterPoints)
 				+ " frequent renter points";
 		return result;
+	}
+
+	private int getFrequentRenterPoints( Rental each) {
+		
+		if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
+				&& each.getDaysRented() > 1)
+			return 2;
+		return 1;
 	}
 
 	private double getAmount( Rental each) {
